e89cf250485d2ce20f5af298df88226d362ec8d9
Ramiro Sebes <ramirosebes@gmail.com>
2023-12-07 12:51:08 -0300

    Validations

--- server.ts
+++ server.ts
@@ -2,12 +2,16 @@ import dotenv from "dotenv";
 import express, { Express } from "express";
 import mongoose from "mongoose";
 import apiRoutes from "./routes/api";
+import cors from 'cors';
+
 dotenv.config();
 
 const app: Express = express();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use("/api", apiRoutes);
 
